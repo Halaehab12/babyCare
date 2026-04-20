e27928567bbe366ff53c30547e0a58e96469324e
Halaehab12 <162878759+Halaehab12@users.noreply.github.com>
2026-04-20 08:08:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+var navbarCollapse = document.getElementById('navbarSupportedContent');
 
-    const btn = document.getElementById("darkMode");
+var navLinks = document.querySelectorAll('.navbar-nav .nav-link:not(.dropdown-toggle)');
 
-    btn.onclick = function() {
-        document.body.classList.toggle("dark")
-    };
+var dropdownItems = document.querySelectorAll('.dropdown-menu .dropdown-item');
 
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        if (navbarCollapse.classList.contains('show')) {
+            new bootstrap.Collapse(navbarCollapse).hide();
+        }
+    });
+});
 
+dropdownItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        if (navbarCollapse.classList.contains('show')) {
+            new bootstrap.Collapse(navbarCollapse).hide();
+        }
+    });
+});
+const btn = document.getElementById("darkMode");
+
+btn.onclick = function () {
+    document.body.classList.toggle("dark");
+};
